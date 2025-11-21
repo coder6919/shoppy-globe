fd5dcf2390e7f1c26b0fcd4480c3a98d02a72a87
Saifshr <ssaifssyed17@gmail.com>
2025-11-21 19:04:33 +0530

    Made checkout component

--- shoppy-globe/src/components/Checkout.jsx
+++ shoppy-globe/src/components/Checkout.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { clearCart, selectCartItems, selectTotalPrice } from '../redux/cartSlice';
